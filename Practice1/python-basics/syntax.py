66e82d9dbe35bda28459b8febba9e34478fde847
Arsen-Kulanov <poprygunchik01@gmail.com>
2026-01-24 02:42:32 +0500

    Add syntax.py examples

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,24 @@
+#This is a comment.
+print("Hello, World!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+# The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+# variables declaration
+x = 5
+y = "Hello, World!"
+# statements
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+# Semicolons are optional in Python. You can write multiple statements on one line by separating them with ; 
+# but this is rarely used because it makes it hard to read
+print("Hello"); print("How are you?"); print("Bye bye!")
