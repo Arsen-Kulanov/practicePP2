a0b33fc7367be7636c9b1716c97f42fa06fc38d2
Arsen-Kulanov <poprygunchik01@gmail.com>
2026-02-01 22:20:01 +0500

    Add boolean topic examples

---
+++ Practice2/boolean/boolean_comparison.py
@@ -0,0 +1,16 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print(True > False)
+print([] == False)
+print((1, 2, 3) == True)
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,7 @@
+a = True
+print(a)
+b = False
+print(b)
+print(bool("Hello"))
+print(bool(15))
+print(bool(0))
