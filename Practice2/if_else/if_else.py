bbdcd70f20bfcb68fab7fc913ccdb3d9d64d3bd5
Arsen-Kulanov <poprygunchik01@gmail.com>
2026-02-03 20:38:31 +0500

    add if-else examples

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,34 @@
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+
+number = 7
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+username = "Emil"
+
+if len(username) > 0:
+    print(f"Welcome, {username}!")
+else:
+    print("Error: Username cannot be empty")
+
+score = 100
+
+if score >= 90:
+    print("Well done")
+else:
+    print("Good")
+
+iscorrect = False
+
+if iscorrect:
+    print("Correct")
+else:
+    print("Incorrect")
